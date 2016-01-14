99dc4868df4fb3fbf7b7ac4c1236dd546db70a22
Manuel Johannes Stegmiller <manuel.stegmiller@uni-ulm.de>
2016-01-14 11:51:03 +0100

    interface der andere gruppe hinzugefügt

--- charm/schemes/por/watersPOR.py
+++ charm/schemes/por/watersPOR.py
@@ -1,11 +1,33 @@
 '''
-Waters und Shacham POR public
-PaperTitle Compact Proofs of Retrievability
+Base class for proof of retrievability
+
+Notes: 4 algorithms: (Kg, St, P, V)
 '''
 
-from charm.toolbox.POR import POR
+from charm.toolbox.schemebase import *
 
-class WatersPOR(POR):
-	
-	def Kg():
-		
+porSchemeType = "PORScheme"
+
+class POR(SchemeBase):
+	def __init__(self):
+		SchemeBase.__init__(self)
+		SchemeBase._setProperty(self, scheme='POR')
+
+	#keygen
+	def Keygen(self):
+		raise NotImplementedError
+
+	#store
+	def Store(self, sk, M):
+		''' return 0 or 1, 1 if the file is stored on the server
+		also returns t and the processed m
+		'''
+		raise NotImplementedError
+
+	#verify
+	def Verify(self, pK, sK, t):
+		raise NotImplementedError
+
+	#prove
+	def Prove(self, pk, t, m):
+		raise NotImplementedError
