c05ae0adbb145456b0617cd6094f254469d8d019
Manuel <moon93@go4more.de>
2016-01-27 13:04:30 +0100

    fixed some syntax stuff

--- charm/schemes/por/PublicProofOfRetrievability.py
+++ charm/schemes/por/PublicProofOfRetrievability.py
@@ -5,25 +5,26 @@ Notes: This class implements an interface for a verifiable proof of retrievabili
 A verifiable proof of retrievability scheme consists of four algorithms: (keygen, generateVerifiers, proove, verify).
 '''
 from charm.core.math.integer import *
+from charm.toolbox.pairinggroup import *
 from random import randint
 
 class VerifiableProofOfRetrievability():
-"""This class implements an interface for verifiable proof of retrievability scheme"""
+	"""This class implements an interface for verifiable proof of retrievability scheme"""
 
-    def __init__(self,bit=80):
+	def __init__(self,bit=80):
 		"""Initializes the class"""
-        self.p =int(integer(randomPrime(bit)))
+		self.p =int(integer(randomPrime(bit)))
 		self.group = PairingGroup('SS512')
-		self.g = group.random(G1)
-		self.u = group.random(G1)
+		self.g = self.group.random(G1)
+		self.u = self.group.random(G1)
 	
-    def _getRandomInt(self,range):
-		"""returns a random integer within 0 and range
+	def _getRandomInt(self,maximum):
+		"""returns a random integer within 0 and maximum
 	
 		args:
 			range: the range within the number should be
 		"""
-        return int(integer(random(range)))    
+		return randint(0,maximum)	
 
 	def setP(self,p):
 		"""allows to set p to a specific value
@@ -33,34 +34,34 @@ class VerifiableProofOfRetrievability():
 		raises:
 			BaseException: if p is not prime
 		"""
-        if isPrime(p):
-           self.p = p
-        else:
-            raise BaseException("p is not Prime")
+		if isPrime(p):
+			self.p = p
+		else:
+			raise BaseException("p is not Prime")
 
-    def keygen(self):
+	def keygen(self):
 		"""generates a public and private key pair"""
 		x = self.group.random(G1) #privateKey
 		v = self.g**x #publicKey
-        return (x, v)
+		return (x, v)
 
-    def splitMessage(self,M):
+	def splitMessage(self,M):
 		"""splits message M in blocks with the length of p
 
 		args:
 			M: the message to be split
 		"""
-        message = []
-        while M!=0:
-             messageBlock = M % self.p
-             message.append(messageBlock)
-             M = M - messageBlock
-             if M!=0:
-                  M=M//int(self.p)
-        message.reverse()
-        return message
+		message = []
+		while M!=0:
+			 messageBlock = M % self.p
+			 message.append(messageBlock)
+			 M = M - messageBlock
+			 if M!=0:
+				  M=M//int(self.p)
+		message.reverse()
+		return message
 
-	def generateSignature(self,splitMessage,i,x)
+	def generateSignature(self,splitMessage,i,x):
 		"""generates s signature
 
 		args:
@@ -72,69 +73,69 @@ class VerifiableProofOfRetrievability():
 		"""
 		#return sigma_i
 		return (self.group.hash(i,G1)*self.u**splitMessage[i])**x
-    
-    def compositeMessage(self, splitMessage):
-    	"""returns one string with the complete message built from the single blocks"""
-        message = 0
-        i=0
-        while i<len(splitMessage)-1: #for m in splitM | geht leider nicht
-             message=(message+splitMessage[i])*self.p
-             i=i+1
-        message=(message+splitMessage[len(splitMessage)-1])
-        return message
+	
+	def compositeMessage(self, splitMessage):
+		"""returns one string with the complete message built from the single blocks"""
+		message = 0
+		i=0
+		while i<len(splitMessage)-1: #for m in splitM | geht leider nicht
+			 message=(message+splitMessage[i])*self.p
+			 i=i+1
+		message=(message+splitMessage[len(splitMessage)-1])
+		return message
 
-    def genChallenge(self,splitMessage,amount):
-    	"""generates a challenge
-    	
-    		args:
-    			splitMessage: list with messageblocks
-    			amount: amount of signatures within the challenge
-    		returns:
-    			challenge: a list of index-factor pairs
-    	"""
-        challange = []
-       	if amount>len(splitMessage):
-       		raise BaseException("Amount bigger than number of message blocks")
-       	for counter in range(0,amount):
-       		i = randint(0,len(splitMessage))
-       		vi = group.random(G1)
-       		challenge.append((i,vi))
-        return challange
-    
-    def proove(self, splitMessage, challenge, x):
-    	"""proove that you can solve the challenge
-    	
-    		args:
-    			splitMessage: list of messageblocks
-    			challenge: challenge got by the verifier
-    			x: private key
-    		returns:
-    			(sigma, y)
-    	"""
+	def genChallenge(self,splitMessage,amount):
+		"""generates a challenge
+		
+			args:
+				splitMessage: list with messageblocks
+				amount: amount of signatures within the challenge
+			returns:
+				challenge: a list of index-factor pairs
+		"""
+		challange = []
+		if amount>len(splitMessage):
+			raise BaseException("Amount bigger than number of message blocks")
+		for counter in range(0,amount):
+				i = randint(0,len(splitMessage))
+				vi = group.random(G1)
+				challenge.append((i,vi))
+		return challange
+	
+	def proove(self, splitMessage, challenge, x):
+		"""proove that you can solve the challenge
+		
+			args:
+				splitMessage: list of messageblocks
+				challenge: challenge got by the verifier
+				x: private key
+			returns:
+				(sigma, y)
+		"""
 		#erzeuge sigma
-    	sigma = 1
-    	for (i,vi) in challenge:
-	    	sigma_i = self.generateSignature(splitMessage,i,x)
-	    	sigma = sigma * (sigma_i**vi)
-	    #erzeuge mü
-	    for (i,vi) in challenge:
-	    	y_i = vi*splitMessage[i]
-	    	y=y+y_i
-        return (sigma, y)
+		sigma = 1
+		for (i,vi) in challenge:
+			sigma_i = self.generateSignature(splitMessage,i,x)
+			sigma = sigma * (sigma_i**vi)
+		#erzeuge mü
+		for (i,vi) in challenge:
+			y_i = vi*splitMessage[i]
+			y=y+y_i
+		return (sigma, y)
 
-    def verify(self, response, challenge, v):
-    	"""verify that the proove is correct
-    	
-    		args:
-    			response: the response gotten from the server/proover
-    			challenge: the used challenge
-    			v: public key of the server
-    		returns:
-    			true if the proove was correct
-    	"""
-    	a = self.group.pair_prod(response[0], self.g)
+	def verify(self, response, challenge, v):
+		"""verify that the proove is correct
+		
+			args:
+				response: the response gotten from the server/proover
+				challenge: the used challenge
+				v: public key of the server
+			returns:
+				true if the proove was correct
+		"""
+		a = self.group.pair_prod(response[0], self.g)
 		temp=1
-    	for (i, vi) in challenge:
-	    	temp =temp* (self.group.hash(i,G1)**vi)*self.u**response[1])
-    	b = self.groupo.pair_prod(temp, v)
-        return a==b
+		for (i, vi) in challenge:
+			temp =temp* ((self.group.hash(i,G1)**vi)*self.u**response[1])
+		b = self.groupo.pair_prod(temp, v)
+		return a==b
